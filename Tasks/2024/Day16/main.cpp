d442cd4ee2ee23ed0136fe38f8f341844c2c7ccc
RedCookieLP <leo.gross.190503@web.de>
2024-12-16 12:10:14 +0100

    Preparation for solving Day16
    I haven't even started yet I'm already scared to solve it... I have a bad reputation with implementing maze-solving-algorithms...

--- Tasks/2024/Day16/main.cpp
+++ Tasks/2024/Day16/main.cpp
@@ -1,16 +1,45 @@
 #include <AoC-Module.h>
+#include <vector>
+#include <Vector2.hpp>
+#include <unordered_map>
+#include <unordered_set>
+#include <array>
 
-void initialize(uint64_t lineCount)
+using Position = Vector2u32;
+
+struct Intersection
 {
+	std::array<Position, 4u> neighbours;
+	Position pos;
+};
 
-}
+static Position s_startPos{};
+static Position s_endPos{};
+static std::unordered_map<Position, Intersection> s_intersections{};
+
+static std::vector<std::pair<bool, Position>> turnHeavyDijkstra(const Position& startPos, const Position& endPos);
 
 bool handleLine(const std::string& line)
 {
-
+	if (line == "")
+		return true;
+	
+	// How would I optimally parse this to get a map of all paths?...
 }
 
 void finalize()
 {
+	std::vector<std::pair<bool, Position>> bestPath = turnHeavyDijkstra(s_startPos, s_endPos);
+	uint64_t deerScore = 0ull;
+	Position lastPos = s_startPos;
+	for (const auto& [mustTurn, newPos] : bestPath)
+	{
+		deerScore += (mustTurn ? 1000ull : 0ull) + (newPos - lastPos).len<uint64_t>() + 1u;
+	}
+	std::cout << "The best score the deer could achieve is " << deerScore << std::endl;
+}
 
+static std::vector<std::pair<bool, Position>> turnHeavyDijkstra(const Position& startPos, const Position& endPos)
+{
+	// TODO: the rest
 }
